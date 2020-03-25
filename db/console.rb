61c06c991a579ff9c2208b47d87da3f66cb327e7
John Taggart <taggartj1985@gmail.com>
2020-03-25 18:22:18 +0000

    show albums, find artists and view all artists and albums working

--- db/console.rb
+++ db/console.rb
@@ -1,5 +1,50 @@
-require("../models/artists")
-require("../models/albums")
+require("pry")
+require_relative("../models/artists")
+require_relative("../models/albums")
+
+Albums.delete_all()
+
+Artists.delete_all()
+
+band1 = Artists.new({'band'=>'RHCP'})
+
+band1.save()
+
+band2 = Artists.new({'band'=>'Stone Sour'})
+
+band2.save()
+
+album1 = Albums.new({'title'=>'One Hot Minute',
+'genre'=>'rock',
+'band_id'=>band1.id
+  })
+
+  album1.save()
+
+album2 = Albums.new({'title'=>'By The WAY',
+'genre'=>'rock',
+'band_id'=>band1.id
+  })
+
+  album2.save()
+
+  album3 = Albums.new({'title'=>'Come What(ever) May',
+  'genre'=>'hard rock',
+  'band_id'=>band2.id
+    })
+
+    album3.save()
 
 
-band1 = Artists.new({})
+band1.show_albums()
+album3.find_artists()
+
+Artists.all()
+Albums.all()
+
+
+
+
+
+binding.pry
+nil
--- db/music_collection.sql
+++ db/music_collection.sql
@@ -2,14 +2,14 @@ DROP TABLE IF EXISTS albums;
 DROP TABLE IF EXISTS artists;
 
 
-CREATE TABLE artists ({
+CREATE TABLE artists (
   id SERIAL PRIMARY KEY,
   band VARCHAR(255)
-  });
+  );
 
-CREATE TABLE albums ({
+CREATE TABLE albums (
   id SERIAL PRIMARY KEY,
   title VARCHAR(255),
   genre VARCHAR(255),
   band_id INT REFERENCES artists(id)
-  });
+  );
